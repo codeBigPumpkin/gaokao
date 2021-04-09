46ae06f0f97a951bea3d92706fc426644ae9e649
jiangfeng2 <Htkg123>
2021-04-09 17:00:11 +0800

    登录

---
+++ src/main/java/com/cn/gaokao/mapper/LoginMapper.java
@@ -0,0 +1,12 @@
+package com.cn.gaokao.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.cn.gaokao.model.User;
+
+/**
+ * @author Administrator
+ * @since 2021/4/9 14:11
+ */
+public interface LoginMapper extends BaseMapper<User> {
+
+}
--- src/main/resources/sql/init-table.sql
+++ src/main/resources/sql/init-table.sql
@@ -20,7 +20,7 @@ SET FOREIGN_KEY_CHECKS=0;
 -- ----------------------------
 DROP TABLE IF EXISTS `tbl_college_info`;
 CREATE TABLE `tbl_college_info` (
-  `id` int(11) NOT NULL COMMENT '学校id',
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '学校id',
   `name` varchar(255) NOT NULL COMMENT '学校名称',
   `address` varchar(255) NOT NULL COMMENT '学校地址',
   `province` varchar(64) DEFAULT NULL COMMENT '学校省份',
@@ -40,7 +40,7 @@ CREATE TABLE `tbl_college_info` (
 -- ----------------------------
 DROP TABLE IF EXISTS `tbl_college_profession`;
 CREATE TABLE `tbl_college_profession` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `college_id` int(11) DEFAULT NULL COMMENT '学校id',
   `profession_id` int(11) DEFAULT NULL COMMENT '专业id',
   `feature` varchar(255) DEFAULT NULL COMMENT '学校专业特色'
@@ -55,7 +55,7 @@ CREATE TABLE `tbl_college_profession` (
 -- ----------------------------
 DROP TABLE IF EXISTS `tbl_college_type_info`;
 CREATE TABLE `tbl_college_type_info` (
-  `oid` int(11) NOT NULL,
+  `oid` int(11) NOT NULL AUTO_INCREMENT,
   `college_id` int(11) NOT NULL COMMENT '学校ID',
   `type_id` int(11) NOT NULL COMMENT '学校类型id   一对多',
   `showtext` varchar(255) DEFAULT NULL COMMENT '显示内容 如果为空显示 type name',
@@ -71,7 +71,7 @@ CREATE TABLE `tbl_college_type_info` (
 -- ----------------------------
 DROP TABLE IF EXISTS `tbl_notice_info`;
 CREATE TABLE `tbl_notice_info` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `type` int(11) DEFAULT NULL COMMENT '公告类型',
   `content` text COMMENT '公告内容',
   `rank` int(11) DEFAULT NULL COMMENT '公告级别',
@@ -88,7 +88,7 @@ CREATE TABLE `tbl_notice_info` (
 -- ----------------------------
 DROP TABLE IF EXISTS `tbl_profession`;
 CREATE TABLE `tbl_profession` (
-  `id` int(11) NOT NULL COMMENT '专业id',
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '专业id',
   `type` varchar(64) DEFAULT NULL COMMENT '专业类型(本科\\专科\\哲学\\经济学\\法学等)',
   `name` varchar(255) DEFAULT NULL COMMENT '专业名称',
   `description` varchar(255) DEFAULT NULL COMMENT '专业描述',
@@ -107,7 +107,7 @@ INSERT INTO `tbl_profession` VALUES ('2', null, '专科专业', null, '0');
 -- ----------------------------
 DROP TABLE IF EXISTS `tbl_province_college_score`;
 CREATE TABLE `tbl_province_college_score` (
-  `id` int(11) DEFAULT NULL,
+  `id` int(11) DEFAULT NOT NULL AUTO_INCREMENT,
   `year` int(11) DEFAULT NULL COMMENT '年份',
   `province` varchar(64) DEFAULT NULL COMMENT '省份',
   `batch` varchar(64) DEFAULT NULL COMMENT '批次(提前批/第一批/第二批/高职等)',
@@ -130,7 +130,7 @@ CREATE TABLE `tbl_province_college_score` (
 -- ----------------------------
 DROP TABLE IF EXISTS `tbl_province_score`;
 CREATE TABLE `tbl_province_score` (
-  `id` int(11) DEFAULT NULL,
+  `id` int(11) DEFAULT NOT NULL AUTO_INCREMENT,
   `year` int(11) DEFAULT NULL COMMENT '年份',
   `province` varchar(64) DEFAULT NULL COMMENT '省份',
   `batch` varchar(64) DEFAULT NULL COMMENT '批次(提前批/第一批/第二批/高职等)',
@@ -148,7 +148,7 @@ CREATE TABLE `tbl_province_score` (
 -- ----------------------------
 DROP TABLE IF EXISTS `tbl_type`;
 CREATE TABLE `tbl_type` (
-  `id` int(11) NOT NULL COMMENT '学校类型ID',
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '学校类型ID',
   `name` varchar(255) NOT NULL DEFAULT '' COMMENT '学校类型名称',
   `ioc` binary(255) DEFAULT NULL COMMENT '类型图标',
   PRIMARY KEY (`id`)
@@ -171,9 +171,9 @@ INSERT INTO `tbl_type` VALUES ('8', '硕士点', null);
 -- ----------------------------
 DROP TABLE IF EXISTS `tbl_user_info`;
 CREATE TABLE `tbl_user_info` (
-  `id` int(11) NOT NULL,
-  `name` varchar(64) NOT NULL COMMENT '用户名字',
-  `phone` int(11) NOT NULL COMMENT '手机号 (登录用)',
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(64) NOT NULL unique COMMENT '用户名字',
+  `phone` bigint(11) NOT NULL unique COMMENT '手机号 (登录用)',
   `userpwd` varchar(255) NOT NULL COMMENT '用户密码',
   `role` int(11) DEFAULT '0' COMMENT '用户类型(会员/艺术/体育/非会员/管理员)',
   `startime` datetime DEFAULT NULL COMMENT '会员开始时间',
@@ -194,7 +194,7 @@ CREATE TABLE `tbl_user_info` (
 -- ----------------------------
 DROP TABLE IF EXISTS `tbl_version_info`;
 CREATE TABLE `tbl_version_info` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `version` varchar(32) DEFAULT NULL COMMENT '版本号',
   `description` varchar(255) DEFAULT NULL COMMENT '版本说明',
   `link` varchar(255) DEFAULT NULL COMMENT '版本地址',
@@ -210,7 +210,7 @@ CREATE TABLE `tbl_version_info` (
 -- ----------------------------
 DROP TABLE IF EXISTS `tbl_video_info`;
 CREATE TABLE `tbl_video_info` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `type` int(11) DEFAULT NULL COMMENT '视频类型',
   `link` varchar(255) DEFAULT NULL COMMENT '视频地址链接',
   `description` varchar(255) DEFAULT NULL,
