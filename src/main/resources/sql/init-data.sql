6b6d1fb5935505b68ea0a1c5d248d8624cc27f25
jiangfeng2 <Htkg123>
2021-04-15 09:03:29 +0800

    sql

---
+++ src/main/resources/sql/init-data.sql
@@ -0,0 +1,43 @@
+-- ----------------------------
+-- ----------------------------
+INSERT INTO `tbl_nature_type` VALUES ('1', '985', null);
+INSERT INTO `tbl_nature_type` VALUES ('2', '211', null);
+INSERT INTO `tbl_nature_type` VALUES ('3', '双一流', null);
+INSERT INTO `tbl_nature_type` VALUES ('4', '双一流学科高校', null);
+INSERT INTO `tbl_nature_type` VALUES ('5', '珠峰计划', null);
+INSERT INTO `tbl_nature_type` VALUES ('6', 'C9', null);
+INSERT INTO `tbl_nature_type` VALUES ('7', '部省共建', null);
+INSERT INTO `tbl_nature_type` VALUES ('8', '卓越大学联盟', null);
+INSERT INTO `tbl_nature_type` VALUES ('9', 'CDIO', null);
+INSERT INTO `tbl_nature_type` VALUES ('10', '卓越工程师', null);
+INSERT INTO `tbl_nature_type` VALUES ('11', '卓越医生', null);
+INSERT INTO `tbl_nature_type` VALUES ('12', '卓越教师', null);
+INSERT INTO `tbl_nature_type` VALUES ('13', '卓越法律', null);
+INSERT INTO `tbl_nature_type` VALUES ('14', '卓越农林', null);
+INSERT INTO `tbl_nature_type` VALUES ('15', '中西部100所', null);
+INSERT INTO `tbl_nature_type` VALUES ('16', '示范高职', null);
+INSERT INTO `tbl_nature_type` VALUES ('17', '骨干高职', null);
+INSERT INTO `tbl_nature_type` VALUES ('18', '华五人', null);
+INSERT INTO `tbl_nature_type` VALUES ('19', '中坚九校', null);
+INSERT INTO `tbl_nature_type` VALUES ('20', '国防七子', null);
+INSERT INTO `tbl_nature_type` VALUES ('21', '四大工学院', null);
+INSERT INTO `tbl_nature_type` VALUES ('22', '两电一邮', null);
+INSERT INTO `tbl_nature_type` VALUES ('23', '两财一贸', null);
+INSERT INTO `tbl_nature_type` VALUES ('24', '六大财经', null);
+INSERT INTO `tbl_nature_type` VALUES ('25', '九大师范', null);
+INSERT INTO `tbl_nature_type` VALUES ('26', '十大医学院', null);
+INSERT INTO `tbl_nature_type` VALUES ('27', '建筑老八校', null);
+INSERT INTO `tbl_nature_type` VALUES ('28', '建筑新八校', null);
+INSERT INTO `tbl_nature_type` VALUES ('29', '法学五院四系', null);
+INSERT INTO `tbl_nature_type` VALUES ('30', '六大农林', null);
+-- ----------------------------
+-- Records of tbl_admission_type
+-- ----------------------------
+-- INSERT INTO `tbl_admission_type` VALUES ('1', '提前本科', null);
+-- INSERT INTO `tbl_admission_type` VALUES ('2', '提前高职(专科)', null);
+-- INSERT INTO `tbl_admission_type` VALUES ('3', '本科一批', null);
+-- INSERT INTO `tbl_admission_type` VALUES ('4', '本科二批', null);
+-- INSERT INTO `tbl_admission_type` VALUES ('5', '本科三批', null);
+-- INSERT INTO `tbl_admission_type` VALUES ('6', '专科批', null);
+-- INSERT INTO `tbl_admission_type` VALUES ('7', '国家专项计划', null);
+-- INSERT INTO `tbl_admission_type` VALUES ('8', '地方专项计划', null);
--- src/main/resources/sql/init-table.sql
+++ src/main/resources/sql/init-table.sql
@@ -16,213 +16,176 @@ Date: 2021-04-05 17:41:10
 SET FOREIGN_KEY_CHECKS=0;
 
 -- ----------------------------
--- Table structure for tbl_college_info
 -- ----------------------------
 DROP TABLE IF EXISTS `tbl_college_info`;
 CREATE TABLE `tbl_college_info` (
-  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '学校id',
+  `id` int NOT NULL AUTO_INCREMENT COMMENT '学校id',
   `name` varchar(255) NOT NULL COMMENT '学校名称',
   `address` varchar(255) NOT NULL COMMENT '学校地址',
   `province` varchar(64) DEFAULT NULL COMMENT '学校省份',
   `city` varchar(128) DEFAULT NULL COMMENT '学校所在城市',
-  `type` varchar(128) DEFAULT NULL COMMENT '学校类型集合 tbl_type',
+  `school_type` varchar(10) DEFAULT NULL COMMENT '院校类别 综合 工科 医药 财经 语言 师范 语言 农业 体育 政法 林业 艺术 民族 军事',
+  `batch_type` varchar(10) DEFAULT NULL COMMENT '批次类别 本科 专科',
+  `nature_type` varchar(10) DEFAULT NULL COMMENT '办学性质 公办 民办 中外合作 港澳合作 独立院校',
+  `institution` varchar(20) DEFAULT NULL COMMENT '所属机构 (教育部 信息化部等等)',
+  `doctoral_program` int DEFAULT NULL COMMENT '博士点',
+  `master_program` int DEFAULT NULL COMMENT '硕士点',
   `description` text COMMENT '学校介绍',
+  `enrollment` text COMMENT '招生简章',
   `ioc` binary(255) DEFAULT NULL COMMENT '学校校徽',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
 
 -- ----------------------------
--- Records of tbl_college_info
 -- ----------------------------
-
+DROP TABLE IF EXISTS `tbl_college_nature_type`;
+CREATE TABLE `tbl_college_nature_type` (
+  `oid` int NOT NULL AUTO_INCREMENT,
+  `college_id` int NOT NULL COMMENT '学校ID',
+  `type_id` int NOT NULL COMMENT '学校类型id   一对多 985  211',
+  `beiyong` varchar(40) DEFAULT NULL COMMENT '备用',
+  PRIMARY KEY (`oid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
 -- ----------------------------
--- Table structure for tbl_college_profession
 -- ----------------------------
 DROP TABLE IF EXISTS `tbl_college_profession`;
 CREATE TABLE `tbl_college_profession` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `college_id` int(11) DEFAULT NULL COMMENT '学校id',
-  `profession_id` int(11) DEFAULT NULL COMMENT '专业id',
-  `feature` varchar(255) DEFAULT NULL COMMENT '学校专业特色',
+  `id` int NOT NULL AUTO_INCREMENT,
+  `college_id` int DEFAULT NULL COMMENT '学校id',
+  `profession_id` int DEFAULT NULL COMMENT '专业id',
+  `paiming` int DEFAULT NULL COMMENT '学校专业排名',
+  `jibie` varchar(10) DEFAULT NULL COMMENT '学校专业级别',
+  `tese` varchar(255) DEFAULT NULL COMMENT '学校专业特色',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
 -- ----------------------------
--- Records of tbl_college_profession
 -- ----------------------------
-
+DROP TABLE IF EXISTS `tbl_nature_type`;
+CREATE TABLE `tbl_nature_type` (
+  `id` int NOT NULL AUTO_INCREMENT COMMENT '学校类型ID',
+  `name` varchar(255) NOT NULL DEFAULT '' COMMENT '建设名称',
+  `ioc` binary(255) DEFAULT NULL COMMENT '类型图标',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
 -- ----------------------------
--- Table structure for tbl_college_type_info
--- ----------------------------
-DROP TABLE IF EXISTS `tbl_college_type_info`;
-CREATE TABLE `tbl_college_type_info` (
-  `oid` int(11) NOT NULL AUTO_INCREMENT,
-  `college_id` int(11) NOT NULL COMMENT '学校ID',
-  `type_id` int(11) NOT NULL COMMENT '学校类型id   一对多',
-  `showtext` varchar(255) DEFAULT NULL COMMENT '显示内容 如果为空显示 type name',
-  PRIMARY KEY (`oid`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- ----------------------------
--- Records of tbl_college_type_info
--- ----------------------------
-
--- ----------------------------
--- Table structure for tbl_notice_info
 -- ----------------------------
 DROP TABLE IF EXISTS `tbl_notice_info`;
 CREATE TABLE `tbl_notice_info` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `type` int(11) DEFAULT NULL COMMENT '公告类型',
+  `id` int NOT NULL AUTO_INCREMENT,
+  `type` int DEFAULT NULL COMMENT '公告类型',
   `content` text COMMENT '公告内容',
-  `rank` int(11) DEFAULT NULL COMMENT '公告级别',
+  `rank` int DEFAULT NULL COMMENT '公告级别',
   `noticetime` datetime DEFAULT NULL COMMENT '公告时间',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
 -- ----------------------------
--- Records of tbl_notice_info
--- ----------------------------
-
--- ----------------------------
--- Table structure for tbl_profession
 -- ----------------------------
 DROP TABLE IF EXISTS `tbl_profession`;
 CREATE TABLE `tbl_profession` (
-  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '专业id',
-  `type` varchar(64) DEFAULT NULL COMMENT '专业类型(本科\\专科\\哲学\\经济学\\法学等)',
+  `id` int NOT NULL AUTO_INCREMENT COMMENT '专业id',
+  `leixing` int DEFAULT NULL COMMENT '专业类型 1 本科 2 专科',
+  `jibie` int DEFAULT NULL COMMENT '专业级别 1 大类 2 小类 3 具体学科',
+  `big_type` varchar(64) DEFAULT NULL COMMENT '专业大类(哲学\\经济学\\法学等)',
+  `small_type` varchar(255) DEFAULT NULL COMMENT '专业小类(法学类、政治学类、社会学类)',
   `name` varchar(255) DEFAULT NULL COMMENT '专业名称',
   `description` varchar(255) DEFAULT NULL COMMENT '专业描述',
-  `pid` int(11) DEFAULT NULL COMMENT '所属专业父专业',
+  `jiuyelv` varchar(40) DEFAULT NULL COMMENT '就业率',
+  `guimo` varchar(40) DEFAULT NULL COMMENT '毕业生规模',
+  `pid` int DEFAULT NULL COMMENT '所属专业父专业',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
 -- ----------------------------
--- Records of tbl_profession
--- ----------------------------
-INSERT INTO `tbl_profession` VALUES ('1', null, '本科专业', null, '0');
-INSERT INTO `tbl_profession` VALUES ('2', null, '专科专业', null, '0');
-
--- ----------------------------
--- Table structure for tbl_province_college_score
 -- ----------------------------
 DROP TABLE IF EXISTS `tbl_province_college_score`;
 CREATE TABLE `tbl_province_college_score` (
-  `id` int(11)  NOT NULL AUTO_INCREMENT,
-  `year` int(11) DEFAULT NULL COMMENT '年份',
+  `id` int NOT NULL AUTO_INCREMENT,
+  `year` int DEFAULT NULL COMMENT '年份',
   `province` varchar(64) DEFAULT NULL COMMENT '省份',
-  `batch` varchar(64) DEFAULT NULL COMMENT '批次(提前批/第一批/第二批/高职等)',
   `type` varchar(64) DEFAULT NULL COMMENT '类型(理科\\文科\\艺术\\体育)',
-  `college_id` int(11) DEFAULT NULL COMMENT '学校id',
-  `rank` int(11) DEFAULT NULL COMMENT '排名',
-  `college_score` int(11) DEFAULT NULL COMMENT '学校录取分数',
-  `profession_score` int(11) DEFAULT NULL COMMENT '学校专业录取分数',
-  `plan_count` int(11) DEFAULT NULL COMMENT '学校计划招生人数',
-  `actual_count` int(11) DEFAULT NULL COMMENT '实际招生人数',
-  `ishistory` bit(1) DEFAULT NULL COMMENT '是否历史',
+  `college_id` int DEFAULT NULL COMMENT '学校id',
+  `batch` int DEFAULT NULL COMMENT '批次 1 提前批 2 本科一批 3 本科二批 4 专科批 5 国家专项计划 6 地方专项计划',
+  `diaodangfen` int DEFAULT NULL COMMENT '调档分',
+  `diaodang_weici` bigint DEFAULT NULL COMMENT '调档分位次',
+  `pingjunfen` int DEFAULT NULL COMMENT '平均分',
+  `pingjun_weici` bigint DEFAULT NULL COMMENT '平均分位次',
+  `plan_count` int DEFAULT NULL COMMENT '计划招生人数',
+  `actual_count` int DEFAULT NULL COMMENT '实际招生人数',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
+-- ----------------------------
+-- ----------------------------
+DROP TABLE IF EXISTS `tbl_province_profession_score`;
+CREATE TABLE `tbl_province_profession_score` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `year` int DEFAULT NULL COMMENT '年份',
+  `province` varchar(64) DEFAULT NULL COMMENT '省份',
+  `type` varchar(64) DEFAULT NULL COMMENT '类型(理科\\文科\\艺术\\体育)',
+  `college_id` int DEFAULT NULL COMMENT '学校id',
+  `batch` int DEFAULT NULL COMMENT '批次 1 提前批 2 本科一批 3 本科二批 4 专科批 5 国家专项计划 6 地方专项计划',
+  `profession_id` bigint DEFAULT NULL COMMENT '专业id',
+  `shilufen` int DEFAULT NULL COMMENT '专业实录分',
+  `shilu_weici` bigint DEFAULT NULL COMMENT '专业实录分位次',
+  `pingjunfen` int DEFAULT NULL COMMENT '专业平均分',
+  `pingjun_weici` bigint DEFAULT NULL COMMENT '专业平均分位次',
+  `plan_count` int DEFAULT NULL COMMENT '专业计划招生人数',
+  `actual_count` int DEFAULT NULL COMMENT '专业实际招生人数',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
 
 -- ----------------------------
--- Records of tbl_province_college_score
--- ----------------------------
-
--- ----------------------------
--- Table structure for tbl_province_score
 -- ----------------------------
 DROP TABLE IF EXISTS `tbl_province_score`;
 CREATE TABLE `tbl_province_score` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `year` int(11) DEFAULT NULL COMMENT '年份',
+  `id` int NOT NULL AUTO_INCREMENT,
+  `year` int DEFAULT NULL COMMENT '年份',
   `province` varchar(64) DEFAULT NULL COMMENT '省份',
   `batch` varchar(64) DEFAULT NULL COMMENT '批次(提前批/第一批/第二批/高职等)',
   `type` varchar(64) DEFAULT NULL COMMENT '类型(理科\\文科\\艺术\\体育)',
-  `score` int(11) DEFAULT NULL COMMENT '分数线',
+  `score` int DEFAULT NULL COMMENT '分数线',
   `ishistory` bit(1) DEFAULT NULL COMMENT '是否历史',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
 -- ----------------------------
--- Records of tbl_province_score
--- ----------------------------
-
--- ----------------------------
--- Table structure for tbl_type
--- ----------------------------
-DROP TABLE IF EXISTS `tbl_type`;
-CREATE TABLE `tbl_type` (
-  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '学校类型ID',
-  `name` varchar(255) NOT NULL DEFAULT '' COMMENT '学校类型名称',
-  `ioc` binary(255) DEFAULT NULL COMMENT '类型图标',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- ----------------------------
--- Records of tbl_type
--- ----------------------------
-INSERT INTO `tbl_type` VALUES ('1', '985', null);
-INSERT INTO `tbl_type` VALUES ('2', '211', null);
-INSERT INTO `tbl_type` VALUES ('3', '本科', null);
-INSERT INTO `tbl_type` VALUES ('4', '公办', null);
-INSERT INTO `tbl_type` VALUES ('5', '理工类', null);
-INSERT INTO `tbl_type` VALUES ('6', '双一流', null);
-INSERT INTO `tbl_type` VALUES ('7', '博士点', null);
-INSERT INTO `tbl_type` VALUES ('8', '硕士点', null);
-
--- ----------------------------
--- Table structure for tbl_user_info
 -- ----------------------------
 DROP TABLE IF EXISTS `tbl_user_info`;
 CREATE TABLE `tbl_user_info` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(64) NOT NULL unique COMMENT '用户名字',
-  `phone` bigint(11) NOT NULL unique COMMENT '手机号 (登录用)',
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(64) NOT NULL COMMENT '用户名字',
+  `phone` bigint NOT NULL COMMENT '手机号 (登录用)',
   `userpwd` varchar(255) NOT NULL COMMENT '用户密码',
-  `role` int(11) DEFAULT '0' COMMENT '用户类型(会员/艺术/体育/非会员/管理员)',
+  `role` int DEFAULT '0' COMMENT '用户类型(会员/艺术/体育/非会员/管理员)',
   `startime` datetime DEFAULT NULL COMMENT '会员开始时间',
   `endtime` datetime DEFAULT NULL COMMENT '到期时间',
   `email` varchar(64) DEFAULT NULL COMMENT '用户邮箱',
   `provice` varchar(64) DEFAULT NULL,
-  `score` int(11) DEFAULT NULL COMMENT '分数',
+  `score` int DEFAULT NULL COMMENT '分数',
   `type` varchar(255) DEFAULT NULL COMMENT '考生类型(理科/文科/艺术/体育)',
-  PRIMARY KEY (`id`,`phone`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
+  PRIMARY KEY (`id`,`phone`),
+  UNIQUE KEY `name` (`name`),
+  UNIQUE KEY `phone` (`phone`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
 -- ----------------------------
--- Records of tbl_user_info
--- ----------------------------
-
--- ----------------------------
--- Table structure for tbl_version_info
 -- ----------------------------
 DROP TABLE IF EXISTS `tbl_version_info`;
 CREATE TABLE `tbl_version_info` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int NOT NULL AUTO_INCREMENT,
   `version` varchar(32) DEFAULT NULL COMMENT '版本号',
   `description` varchar(255) DEFAULT NULL COMMENT '版本说明',
   `link` varchar(255) DEFAULT NULL COMMENT '版本地址',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
 -- ----------------------------
--- Records of tbl_version_info
--- ----------------------------
-
--- ----------------------------
--- Table structure for tbl_video_info
 -- ----------------------------
 DROP TABLE IF EXISTS `tbl_video_info`;
 CREATE TABLE `tbl_video_info` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `type` int(11) DEFAULT NULL COMMENT '视频类型',
+  `id` int NOT NULL AUTO_INCREMENT,
+  `type` int DEFAULT NULL COMMENT '视频类型',
   `link` varchar(255) DEFAULT NULL COMMENT '视频地址链接',
   `description` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
 
--- ----------------------------
--- Records of tbl_video_info
--- ----------------------------
+
 
 
 -- 测试表
